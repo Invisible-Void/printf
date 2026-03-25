611a13f3adfb1efa9b0d9fa680e2c7bd841a96cd
invisiblevoid <invisiblevoid@protonmail.com>
2026-03-25 22:28:06 +0100

    added a strcpy function to utils

--- include/utils.h
+++ include/utils.h
@@ -3,6 +3,7 @@
 #include <stddef.h>
 
 size_t strlen(const char* str);
+void strcpy(char* buffer, const char* str, size_t length);
 
 
 #endif // UTILS_H
--- src/utils.c
+++ src/utils.c
@@ -9,4 +9,14 @@ size_t strlen(const char* str) {
     return count;
 }
 
+// copies str to buffer with length-1
+// length should be the size of the buffer or string to copy with null byte included
+void strcpy(char* buffer, const char* str, size_t length) {
+    size_t i;
+    for (i = 0; i < length-1 && str[i] != '\0'; i++) {
+        buffer[i] = str[i];
+    }
+    buffer[i] = '\0';
+}
+
 
