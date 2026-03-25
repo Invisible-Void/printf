752f6e2e00acf995598ab8789eef833f25344517
invisiblevoid <invisiblevoid@protonmail.com>
2026-03-25 22:28:52 +0100

    added a buffer and a strcpy from message to buffer

--- src/printf.c
+++ src/printf.c
@@ -1,7 +1,11 @@
 #include <printf.h>
 
 void printf(const char* message, ...) {
+    char buffer[4096]; // i need some space and i can't use malloc :(
     size_t length = strlen(message);
-    sys_write(1, message, length);
+
+    strcpy(buffer, message, 4096);
+
+    sys_write(1, buffer, length);
 }
 
